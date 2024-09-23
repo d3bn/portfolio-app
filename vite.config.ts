16bbbaea63434fa8287664517ffcf39841f8b9b8
d3bn <me@dennisbasan.com>
2024-09-23 18:04:48 +0800

    feat: add gh pages package for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     fs: {
       // Allow case-insensitive file system access
       strict: false,
     },
   },
+  build: {
+    outDir: 'build',
+    emptyOutDir: true, // also necessary
+  }
 });
