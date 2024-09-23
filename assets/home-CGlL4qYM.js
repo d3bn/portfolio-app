212bfdebd57320cee42e3db614706adadf536f5f
d3bn <d3bn@users.noreply.github.com>
2024-09-23 18:18:25 +0000

    deploy: 713efd999c52d8b562e0844d93aa9163b7e15914

---
+++ assets/home-CGlL4qYM.js
@@ -0,0 +1,9 @@
+import{j as i,a as e,c as n,L as r}from"./index-By1vQ_Jp.js";function o(){return i("div",{css:t,children:[e("h1",{children:"Hi, I'm Dennis."}),i("p",{children:["I am a Senior Software Engineer specialize in building exceptional"," ",e("strong",{children:"web"})," and ",e("strong",{children:"mobile"})," applications with cutting-edge web technologies. Let's collaborate on your next project and create a seamless user experience across all devices."]}),i("p",{children:["Currently i work at ",e(r,{target:"_blank",rel:"noopener noreferrer",to:"https://staizen.com",children:e("strong",{children:"Staizen"})}),", as part of Frontend Community building web application for different companies we are working with."]})]})}const t=n`
+  display: flex;
+  flex-direction: column;
+  line-height: 1.8em;
+  
+  @media (min-width: 1024px) {
+    width: calc(100vw / 3);
+  }
+`;export{o as default};
---
+++ assets/not-found-JTMt92CT.js
@@ -0,0 +1,15 @@
+import{j as s,a as e,l as i,c as n,R as a,L as t}from"./index-By1vQ_Jp.js";function l(){return s("div",{css:o,children:[e("img",{src:i,css:c,alt:"Dennis Basan"}),e("h3",{children:"Daddy, chill."}),e("p",{children:"The page you seek is in another castle. Keep exploring!"}),s("p",{children:["Open this ",e(t,{to:a.HOME,children:"Door"}),"."]})]})}const o=n`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.1em;
+  height: 100%;
+
+  h3, p {
+    margin: 0.3em;
+  }
+`,c=n`
+  width: ${150/16}em;
+  padding: 1em;
+`;export{l as default};
