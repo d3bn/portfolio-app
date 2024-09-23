c8216797ce581d8097263d4d7f87585dcf835837
d3bn <d3bn@users.noreply.github.com>
2024-09-23 19:53:44 +0000

    deploy: b721ecb58a55bfb4dcfc3d503ff78daf1c639090

---
+++ assets/error-found-CdrXjaiy.js
@@ -0,0 +1,26 @@
+import{j as i,a as e,l as o,c as s}from"./index-NOPZYabi.js";function m({error:n,resetErrorBoundary:r}){return i("div",{css:a,children:[e("img",{src:o,css:c,alt:"Error Page!",loading:"lazy"}),e("h3",{children:"The page has been assimilated. Resistance is futile."}),e("p",{css:l,children:n.message}),e("button",{onClick:t=>{t.preventDefault(),r()},css:d,children:e("strong",{children:"Destroy"})}),"."]})}const a=s`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.1em;
+  height: 100%;
+
+  h3,
+  p {
+    margin: 0.3em;
+  }
+`,c=s`
+  width: ${150/16}em;
+  padding: 1em;
+`,l=s`
+  max-width: 50em;
+  text-align: center;
+`,d=s`
+  background: none;
+  border: none;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+  font: inherit;
+`;export{m as default};
---
+++ assets/not-found-cjfkDgVq.js
@@ -0,0 +1,15 @@
+import{j as s,a as e,l as i,c as n,R as a,L as o}from"./index-NOPZYabi.js";function r(){return s("div",{css:t,children:[e("img",{src:i,css:l,alt:"Dennis Basan",loading:"lazy"}),e("h3",{children:"Daddy, chill."}),e("p",{children:"The page you seek is in another castle. Keep exploring!"}),s("p",{children:["Open this ",e(o,{to:a.HOME,children:"Door"}),"."]})]})}const t=n`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.1em;
+  height: 100%;
+
+  h3, p {
+    margin: 0.3em;
+  }
+`,l=n`
+  width: ${150/16}em;
+  padding: 1em;
+`;export{r as default};
