9490510dfdd13d5f7440f17a9504fb2212d98205
d3bn <d3bn@users.noreply.github.com>
2025-06-14 10:30:14 +0000

    deploy: 16d94672eff58089d0b0973f44c242a7dd87e227

---
+++ assets/home-E7L573ok.js
@@ -0,0 +1,20 @@
+import{j as n,a as e,L as t,c as i}from"./index-Cu7BK1Li.js";const r="/assets/banner-BomnGMlQ.svg";function l(){return n("div",{css:s,children:[n("div",{css:o,children:[e("h1",{children:"Hi, I'm Dennis."}),n("p",{children:["I am a Senior Software Engineer specialize in building ",e("strong",{children:"web"})," and ",e("strong",{children:"mobile"})," applications with cutting-edge web technologies. From the mesmerizing frontend to the hidden depths of backend implementation, Currently i work at"," ",e(t,{target:"_blank",rel:"noopener noreferrer",to:"https://staizen.com",children:e("strong",{children:"Staizen"})}),", as part of Frontend Community building web application for different companies we are working with."]})]}),e("img",{src:r,title:"Dennis Basan | Software Developer",css:a})]})}const s=i`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3em;
+
+  > * {
+    flex: 1 1 0px;
+  }
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+`,o=i`
+  display: flex;
+  flex-direction: column;
+  line-height: 1.8em;
+`,a=i`
+  width: 100%;
+`;export{l as default};
