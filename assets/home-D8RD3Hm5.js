dba4ac3e41337ca8645a6d1fbf435e9bce4eaeeb
d3bn <d3bn@users.noreply.github.com>
2025-10-26 05:33:19 +0000

    deploy: bd798c86161d7168ab0ace91b914e37b81ff8d31

---
+++ assets/home-D8RD3Hm5.js
@@ -0,0 +1,20 @@
+import{j as n,a as e,L as t,c as i}from"./index-CGlqTSs1.js";const r="/assets/banner-BomnGMlQ.svg";function c(){return n("div",{css:a,children:[n("div",{css:s,children:[e("h1",{children:"Hi, I'm Dennis."}),n("p",{children:["I am a Senior Software Engineer specialize in building"," ",e("strong",{children:"web"})," and ",e("strong",{children:"mobile"})," applications with cutting-edge web technologies. From the mesmerizing frontend to the hidden depths of backend implementation, Currently i work at"," ",e(t,{target:"_blank",rel:"noopener noreferrer",to:"https://pragma.team",children:e("strong",{children:"pragma.team"})}),", as part of Software Engineering community building web and mobile applications for different companies."]})]}),e("img",{src:r,title:"Dennis Basan | Software Developer",alt:"Dennis Basan | Software Developer Banner",css:o})]})}const a=i`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3em;
+
+  > * {
+    flex: 1 1 0px;
+  }
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+`,s=i`
+  display: flex;
+  flex-direction: column;
+  line-height: 1.8em;
+`,o=i`
+  width: 100%;
+`;export{c as default};
